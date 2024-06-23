b2924771fdcb93638d25afb48559d10a271a6a60
wgs153 <wgs15313468172@163.com>
2024-06-23 11:42:16 +0800

    课程添加

--- tienchin-admin/src/main/java/org/javaboy/tienchin/web/controller/tienchin/CourseController.java
+++ tienchin-admin/src/main/java/org/javaboy/tienchin/web/controller/tienchin/CourseController.java
@@ -1,16 +1,18 @@
 package org.javaboy.tienchin.web.controller.tienchin;
 
 import org.javaboy.tienchin.activity.domain.vo.ActivityVO;
+import org.javaboy.tienchin.activity.validator.CreateGroup;
+import org.javaboy.tienchin.common.annotation.Log;
 import org.javaboy.tienchin.common.core.controller.BaseController;
 import org.javaboy.tienchin.common.core.domain.AjaxResult;
 import org.javaboy.tienchin.common.core.page.TableDataInfo;
+import org.javaboy.tienchin.common.enums.BusinessType;
 import org.javaboy.tienchin.course.domain.Course;
+import org.junit.jupiter.params.shadow.com.univocity.parsers.annotations.Validate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 import org.javaboy.tienchin.course.service.ICourseService;
 import java.util.List;
 
@@ -25,7 +27,6 @@ import java.util.List;
 @RestController
 @RequestMapping("/tienchin/course")
 public class CourseController extends BaseController {
-
     @Autowired
     ICourseService courseService;
 
@@ -41,8 +42,9 @@ public class CourseController extends BaseController {
     }
 
     @PreAuthorize("@ss.hasPermi('tienchin:course:create')")
-    @PostMapping("/add")
-    public AjaxResult addCourse(Course course){
+    @Log(title = "课程管理", businessType = BusinessType.INSERT)
+    @PostMapping()
+    public AjaxResult addCourse(@Validated @RequestBody Course course){
 
 
         return courseService.addCourse(course);
--- tienchin-course/src/main/java/org/javaboy/tienchin/course/domain/Course.java
+++ tienchin-course/src/main/java/org/javaboy/tienchin/course/domain/Course.java
@@ -3,6 +3,10 @@ package org.javaboy.tienchin.course.domain;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 
@@ -25,20 +29,26 @@ public class Course implements Serializable {
     /**
      * 1:舞蹈类 2:游泳类 3:拳击类
      */
+    @NotNull(message = "{course.type.NotNull}")
     private Integer type;
 
+    @NotBlank(message = "{course.name.NotBlank}")
     private String name;
 
+    @NotNull(message = "{course.price.NotNull}")
+    @Min(value = 0,message = "{course.price.Min}")
     private Double price;
 
     /**
      * 课程适用人群
      */
+    @NotNull(message = "{course.applyTo.NotBlank}")
     private Integer applyTo;
 
     /**
      * 课程简介
      */
+    @NotBlank(message = "{course.info.NotBlank}")
     private String info;
 
     /**
